e798b107317e3b8dd4d1272f4623396db1e639a5
JEGATHEESWARAN Nithujan <Nithujan.JEGATHEESWARAN@cpnv.ch>
2022-02-07 12:20:10 +0100

    test code for Flask-SQLAlchemy (start)

--- code/disposable_code/app.py
+++ code/disposable_code/app.py
@@ -4,10 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Pa$$w0rd@localhost/snows'
-db.create_all(app)
 
 
 @app.route('/')
 def get_data():
-    db
-    return render_template('db_test.html', )
+    snows = db.select('SELECT * FROM snows')
+    return render_template('db_test.html', snows)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
