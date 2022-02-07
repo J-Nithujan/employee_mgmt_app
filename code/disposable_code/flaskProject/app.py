30f98f2f88c33fda33cb1dbb8aca06c4c4ca1c2c
J-Nithujan <nithujan.jegatheeswaran@cpnv.ch>
2022-02-07 19:47:42 +0100

    code folder organisation

---
+++ code/disposable_code/flaskProject/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, redirect, url_for, request, render_template, session
+
+app = Flask(__name__)
+app.secret_key = 'dfaljfj'
+
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    # Helps in handling data in URL
+    return 'Hello %s!' % name
+
+
+@app.route('/admin')
+def hello_admin():
+    return 'Hello Admin'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return 'Hello %s as Guest' % guest
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest=name))
+
+
+@app.route('/success/<name>')
+def success(name):
+    session['username'] = name
+    return render_template('main_menu.html', username=name)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        # for the GET method
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return render_template('logout.html', username=session['username'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
