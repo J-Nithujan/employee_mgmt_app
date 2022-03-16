52b72636e5b101068905094d8eb8e01c50b5f042
JEGATHEESWARAN Nithujan <Nithujan.JEGATHEESWARAN@cpnv.ch>
2022-03-16 11:03:38 +0100

    another query method test

--- code/employees_mgmt_app/app/model/utils.py
+++ code/employees_mgmt_app/app/model/utils.py
@@ -1,24 +1,32 @@
 from app.model.tables import Tables, db
+from sqlalchemy.sql import *
 import hashlib
 
 
 def check_login(email: str, password: str) -> bool:
     query_tables = Tables()
+    employees = query_tables.employees
     encoded = password.encode()
     hashed = hashlib.sha256(encoded)
-    result = db.session.query(query_tables.employees.c.email, query_tables.employees.c.password).select_from(
-        query_tables.employees).filter(
-        query_tables.employees.c.email == email, query_tables.employees.c.password == hashed.hexdigest()).all()
-    if len(result) == 1:
+    # result = db.session.query(employees.c.email, employees.c.password).select_from(
+    #     employees).filter(
+    #     employees.c.email == email, employees.c.password == hashed.hexdigest()).all()
+    
+    stmt = select(employees.c.email, employees.c.password).where(employees.c.email == email,
+                                                                 employees.c.password == hashed.hexdigest())
+    with db.engine.connect() as connection:
+        result = connection.execute(stmt)
+    
+    if len(result.all()) == 1:
         return True
     else:
         return False
 
-
-def get_index_data(email: str) -> list[tuple]:
-    query_tables: Tables = Tables()
-    data: tuple = db.session.query(query_tables.employees.c.firstname, query_tables.employees.c.lastname,
-                                   query_tables.employees.c.birthdate, query_tables.employees.c.road, query_tables.employees.c.phone_number, query_tables.employees.c.hiring_date, query_tables.employees.c.).select_from(query_tables.employees).filter(
-        query_tables.employees.c.email == email)
-    return data
-    pass
+# def get_index_data(email: str) -> list[tuple]:
+#     # return type ????
+#     query_tables: Tables = Tables()
+#     data: list[tuple]  = db.session.query(query_tables.employees.c.firstname, query_tables.employees.c.lastname,
+#                                    query_tables.employees.c.birthdate, query_tables.employees.c.road, query_tables.employees.c.phone_number, query_tables.employees.c.hiring_date, query_tables.employees.c.job, query_tables.employees.c.percentage).select_from(query_tables.employees).filter(
+#         query_tables.employees.c.email == email)
+#     return data
+#     pass
