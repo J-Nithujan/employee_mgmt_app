391c38adc6120107c3154ec7c5bd64f6d46121e5
J-Nithujan <nithujan.jegatheeswaran@cpnv.ch>
2022-03-20 20:11:12 +0100

    model files update
    - one class per table in the database
    - utils function: index_data() and get_tasks()

--- code/employees_mgmt_app/app/model/tables.py
+++ code/employees_mgmt_app/app/model/tables.py
@@ -1,15 +1,31 @@
+# File: tables.py
+# Author: Nithujan Jegatheeswaran
+# Brief: 
+# Version: 19.03.2022
+
 from flask_sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 
 
+class Addresses(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    zip = db.Column(db.VARCHAR, nullable=False)
+    city = db.Column(db.VARCHAR, nullable=False)
+
+
+class Departments(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.VARCHAR)
+
+
 class Employees(db.Model):
-    __tablename__ = 'employees'
-    
-    id = db.Column('id', db.Integer, autoincrement=True, primary_key=True)
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     email = db.Column(db.VARCHAR, nullable=False)
-    first_name = db.Column(db.VARCHAR, nullable=False)
-    last_name = db.Column(db.VARCHAR, nullable=False)
+    firstname = db.Column(db.VARCHAR, nullable=False)
+    lastname = db.Column(db.VARCHAR, nullable=False)
     birthdate = db.Column(db.DATE, nullable=False)
     phone_number = db.Column(db.VARCHAR)
     road = db.Column(db.VARCHAR, nullable=False)
@@ -24,22 +40,52 @@ class Employees(db.Model):
     address_id = db.Column(db.Integer, nullable=False)
     job_id = db.Column(db.Integer, nullable=False)
     department_id = db.Column(db.Integer, nullable=False)
-    
 
-class Tables:
-    employees: db.Table = None
-    payslips: db.Table = None
-    addresses: db.Table = None
-    jobs: db.Table = None
-    departments: db.Table = None
-    tasks: db.Table = None
-    employee_has_task: db.Table = None
 
-    def __init__(self):
-        self.employees = db.Table('employees', db.metadata, autoload_with=db.engine)
-        self.payslips = db.Table('payslips', db.metadata, autoload_with=db.engine)
-        self.addresses = db.Table('addresses', db.metadata, autoload_with=db.engine)
-        self.jobs = db.Table('jobs', db.metadata, autoload_with=db.engine)
-        self.departments = db.Table('departments', db.metadata, autoload_with=db.engine)
-        self.tasks = db.Table('tasks', db.metadata, autoload_with=db.engine)
-        self.employee_has_task = db.Table('employee_has_task', db.metadata, autoload_with=db.engine)
+class Employee_has_task(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    employee_id = db.Column(db.Integer, nullable=False)
+    task_id = db.Column(db.Integer, nullable=False)
+
+
+class Jobs(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.VARCHAR, nullable=False)
+
+
+class Payslips(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    file_path = db.Column(db.VARCHAR, nullable=False)
+    date = db.Column(db.DATE, nullable=False)
+    employee_id = db.Column(db.Integer, nullable=False)
+
+
+class Tasks(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    project = db.Column(db.VARCHAR, nullable=False)
+    title = db.Column(db.VARCHAR, nullable=False)
+    description = db.Column(db.TEXT, nullable=False)
+    validation = db.Column(db.BOOLEAN, nullable=False)
+    since = db.Column(db.DATETIME, nullable=False)
+    until = db.Column(db.DATETIME, nullable=False)
+
+
+# INFO: This cannot be used for inner joins
+# ---
+# class Tables:
+#     employees: db.Table = None
+#     payslips: db.Table = None
+#     addresses: db.Table = None
+#     jobs: db.Table = None
+#     departments: db.Table = None
+#     tasks: db.Table = None
+#     employee_has_task: db.Table = None
+#
+#     def __init__(self):
+#         self.employees = db.Table('employees', db.metadata, autoload_with=db.engine)
+#         self.payslips = db.Table('payslips', db.metadata, autoload_with=db.engine)
+#         self.addresses = db.Table('addresses', db.metadata, autoload_with=db.engine)
+#         self.jobs = db.Table('jobs', db.metadata, autoload_with=db.engine)
+#         self.departments = db.Table('departments', db.metadata, autoload_with=db.engine)
+#         self.tasks = db.Table('tasks', db.metadata, autoload_with=db.engine)
+#         self.employee_has_task = db.Table('employee_has_task', db.metadata, autoload_with=db.engine)
--- code/employees_mgmt_app/app/model/utils.py
+++ code/employees_mgmt_app/app/model/utils.py
@@ -1,50 +1,54 @@
-from app.model.tables import Tables, db, Employees
+# File: utils.py
+# Author: Nithujan Jegatheeswaran
+# Brief: 
+# Version: 19.03.2022
+
+from app.model.tables import *
 from sqlalchemy.sql import *
 from sqlalchemy.orm import aliased
 import hashlib
 
 
 def check_login(email: str, password: str) -> bool:
-    query_tables = Tables()
-    employees = query_tables.employees
     encoded = password.encode()
     hashed = hashlib.sha256(encoded)
-    # result = db.session.query(employees.c.email, employees.c.password).select_from(
-    #     employees).filter(
-    #     employees.c.email == email, employees.c.password == hashed.hexdigest()).all()
-    
-    stmt = select(Employees.email, Employees.password).where(Employees.email == email,
-                                                             Employees.password == hashed.hexdigest())
-    with db.engine.connect() as connection:
-        result = connection.execute(stmt)
-    
-    if len(result.all()) == 1:
+    stmt = select(Employees.email).where(Employees.email == email,
+                                         Employees.password == hashed.hexdigest())
+    result = db.session.execute(stmt).all()
+
+    if len(result) == 1:
         return True
     else:
         return False
 
 
-def get_index_data(email: str) -> list[tuple]:
-    # return type ????
-    query_tables: Tables = Tables()
+def get_index_data(email: str) -> list:
     supervisor = aliased(Employees)
-    # Correct this statement on the join part
-    # stmt = select(Employees.first_name, Employees.last_name,
-    #               Employees.birthdate, Employees.road,
-    #               Employees.phone_number, Employees.hiring_date,
-    #               query_tables.jobs.c.name, Employees.percentage,
-    #               supervisor.first_name, supervisor.last_name).join(Employees, query_tables.jobs).join(Employees, supervisor)
-    data: list = list()
-    with db.engine.connect() as conn:
-        for row in conn.execute(stmt):
-            data = row
+    # TODO: select * == safe or not ?
+    query = select(Employees.firstname, Employees.lastname, Employees.birthdate, Employees.road, Addresses.zip,
+                   Addresses.city, Employees.phone_number, Employees.hiring_date, Employees.percentage,
+                   Departments.name.label('department'),
+                   supervisor.firstname.label('supervisor_firstname'),
+                   supervisor.lastname.label('supervisor_lastname')).\
+        join(supervisor, supervisor.id == Employees.employee_id).\
+        join(Addresses, Employees.address_id == Addresses.id).\
+        join(Departments, Employees.department_id == Departments.id).\
+        where(Employees.email == email)
 
-    # data: list[tuple] = db.session.query(query_tables.employees.c.firstname, query_tables.employees.c.lastname,
-    #                                      query_tables.employees.c.birthdate, query_tables.employees.c.road,
-    #                                      query_tables.employees.c.phone_number, query_tables.employees.c.hiring_date,
-    #                                      query_tables.jobs.c.name, query_tables.employees.c.percentage,
-    #                                      query_tables.employees.c.employee_id).select_from(
-    #     query_tables.employees).join(query_tables.jobs, query_tables.employees).filter(
-    #     query_tables.employees.c.email == email)
-    return data
+    data = db.session.execute(query).all()
+    return data[0]
+
+
+# TODO: get_tasks  fct
+def get_tasks(email):
+    query = select(Tasks.project, Tasks.title, Tasks.since, Tasks.until).\
+        join(Employee_has_task, Tasks.id == Employee_has_task.task_id).\
+        join(Employees, Employee_has_task.employee_id == Employees.id).where(Employees.email == email)
+
+    tasks = db.session.execute(query).all()
+    return tasks
+
+
+# TODO: add_task fct
+def add_task(post):
     pass
