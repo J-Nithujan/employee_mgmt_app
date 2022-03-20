b20fabc717d5231c16f179f17d4aaaf90d1bcb01
J-Nithujan <nithujan.jegatheeswaran@cpnv.ch>
2022-03-20 19:52:44 +0100

    db conception minor changes
    mld:
     - Employees: password cannot be null anymore, application will give a default value
     - on delete / update : no action for all fk

--- data/db-tables-creation-with-drop.sql
+++ data/db-tables-creation-with-drop.sql
@@ -5,9 +5,6 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema db_employees
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `db_employees` ;
@@ -29,24 +26,7 @@ CREATE TABLE IF NOT EXISTS `db_employees`.`addresses` (
   `city` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `address_unique` (`ZIP` ASC, `city` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 51
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `db_employees`.`departments`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `db_employees`.`departments` ;
-
-CREATE TABLE IF NOT EXISTS `db_employees`.`departments` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 13
-DEFAULT CHARACTER SET = utf8mb3;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -59,9 +39,20 @@ CREATE TABLE IF NOT EXISTS `db_employees`.`jobs` (
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 21
-DEFAULT CHARACTER SET = utf8mb3;
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_employees`.`departments`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_employees`.`departments` ;
+
+CREATE TABLE IF NOT EXISTS `db_employees`.`departments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -80,10 +71,10 @@ CREATE TABLE IF NOT EXISTS `db_employees`.`employees` (
   `hiring_date` DATE NOT NULL,
   `percentage` DECIMAL(5,2) NOT NULL,
   `salary` DECIMAL(7,2) NOT NULL,
-  `holiday_balance` DECIMAL(4,2) NOT NULL DEFAULT '0.00',
+  `holiday_balance` DECIMAL(4,2) NOT NULL DEFAULT 0,
   `under_contract` TINYINT NOT NULL,
-  `work_time` DECIMAL(10,0) NOT NULL DEFAULT '0',
-  `password` VARCHAR(64) CHARACTER SET 'utf8' NULL DEFAULT NULL,
+  `work_time` DECIMAL NOT NULL DEFAULT 0,
+  `password` VARCHAR(64) NOT NULL,
   `employee_id` INT NULL DEFAULT NULL,
   `address_id` INT NOT NULL,
   `job_id` INT NOT NULL,
@@ -94,64 +85,27 @@ CREATE TABLE IF NOT EXISTS `db_employees`.`employees` (
   INDEX `fk_employees_addresses1_idx` (`address_id` ASC) VISIBLE,
   INDEX `fk_employees_job1_idx` (`job_id` ASC) VISIBLE,
   INDEX `fk_employees_departments1_idx` (`department_id` ASC) VISIBLE,
-  CONSTRAINT `fk_employees_addresses1`
-    FOREIGN KEY (`address_id`)
-    REFERENCES `db_employees`.`addresses` (`id`),
-  CONSTRAINT `fk_employees_departments1`
-    FOREIGN KEY (`department_id`)
-    REFERENCES `db_employees`.`departments` (`id`),
   CONSTRAINT `fk_employees_employees`
     FOREIGN KEY (`employee_id`)
-    REFERENCES `db_employees`.`employees` (`id`),
+    REFERENCES `db_employees`.`employees` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_employees_addresses1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `db_employees`.`addresses` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_employees_job1`
     FOREIGN KEY (`job_id`)
-    REFERENCES `db_employees`.`jobs` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 52
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `db_employees`.`tasks`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `db_employees`.`tasks` ;
-
-CREATE TABLE IF NOT EXISTS `db_employees`.`tasks` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `project` VARCHAR(45) NOT NULL,
-  `title` VARCHAR(45) NOT NULL,
-  `description` TINYTEXT NULL DEFAULT NULL,
-  `validation` TINYINT(1) NULL DEFAULT '0',
-  `since` DATETIME NOT NULL,
-  `until` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `unique_tasks` (`project` ASC, `title` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 51
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `db_employees`.`employee_has_task`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `db_employees`.`employee_has_task` ;
-
-CREATE TABLE IF NOT EXISTS `db_employees`.`employee_has_task` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `employee_id` INT NOT NULL,
-  `task_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_employees_has_tasks_tasks1_idx` (`task_id` ASC) VISIBLE,
-  INDEX `fk_employees_has_tasks_employees1_idx` (`employee_id` ASC) VISIBLE,
-  CONSTRAINT `fk_employee_has_task_employees1`
-    FOREIGN KEY (`employee_id`)
-    REFERENCES `db_employees`.`employees` (`id`),
-  CONSTRAINT `fk_employee_has_task_tasks1`
-    FOREIGN KEY (`task_id`)
-    REFERENCES `db_employees`.`tasks` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 51
-DEFAULT CHARACTER SET = utf8mb3;
+    REFERENCES `db_employees`.`jobs` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_employees_departments1`
+    FOREIGN KEY (`department_id`)
+    REFERENCES `db_employees`.`departments` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -169,10 +123,53 @@ CREATE TABLE IF NOT EXISTS `db_employees`.`payslips` (
   INDEX `fk_payslips_employees1_idx` (`employee_id` ASC) VISIBLE,
   CONSTRAINT `fk_payslips_employees1`
     FOREIGN KEY (`employee_id`)
-    REFERENCES `db_employees`.`employees` (`id`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 51
-DEFAULT CHARACTER SET = utf8mb3;
+    REFERENCES `db_employees`.`employees` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_employees`.`tasks`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_employees`.`tasks` ;
+
+CREATE TABLE IF NOT EXISTS `db_employees`.`tasks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `project` VARCHAR(45) NOT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `description` TINYTEXT NULL DEFAULT NULL,
+  `validation` TINYINT(1) NULL DEFAULT 0,
+  `since` DATETIME NOT NULL,
+  `until` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `unique_tasks` (`project` ASC, `title` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_employees`.`employee_has_task`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_employees`.`employee_has_task` ;
+
+CREATE TABLE IF NOT EXISTS `db_employees`.`employee_has_task` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `employee_id` INT NOT NULL,
+  `task_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_employees_has_tasks_tasks1_idx` (`task_id` ASC) VISIBLE,
+  INDEX `fk_employees_has_tasks_employees1_idx` (`employee_id` ASC) VISIBLE,
+  CONSTRAINT `fk_employee_has_task_employees1`
+    FOREIGN KEY (`employee_id`)
+    REFERENCES `db_employees`.`employees` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_employee_has_task_tasks1`
+    FOREIGN KEY (`task_id`)
+    REFERENCES `db_employees`.`tasks` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
