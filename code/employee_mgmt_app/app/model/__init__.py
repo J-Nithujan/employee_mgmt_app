1f8c511dd1dcb06f47537fb122a45d5bd9e5d618
JEGATHEESWARAN Nithujan <Nithujan.JEGATHEESWARAN@cpnv.ch>
2022-04-11 10:59:56 +0200

    merge

---
+++ code/employee_mgmt_app/app/model/__init__.py
@@ -0,0 +1,4 @@
+# File: __init__.py
+# Author: Nithujan Jegatheeswaran
+# Brief: This package contains all the class for the database's table and the functions to manipulate them
+# Version: 30.03.2022
